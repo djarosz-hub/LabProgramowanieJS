b8baa12bcf0895d797fef39fb43b9802e869e00c
djarosz-hub <jaroszdamian9@gmail.com>
2020-10-14 21:28:23 +0200

    functionalities with bugs added

--- cw1_lightbox/main.js
+++ cw1_lightbox/main.js
@@ -5,19 +5,60 @@ for(let i = 0; i < gallery.length ;i++){
     const img = gallery[i];
     img.addEventListener('click',showLightbox);
 }
-
-// gallery.addEventListener('click', showLightbox);
+const arrowPrevDiv = document.querySelector('.arrowprev');
+const arrowNextDiv = document.querySelector('.arrownext');
+//const arrowNext = document.querySelector('.arrownext i');
+//const arrowPrev = document.querySelector('.arrowprev i');
 function showLightbox(ev){
-    console.log(ev.target);
+    console.dir(ev.target);
     const lightbox = document.querySelector('.lightbox');
     const img = document.querySelector('.lightbox img');
     const imgUrl = ev.target.src;
     img.src = imgUrl;
     lightbox.classList.add('visible');
+    // const nextElement = ev.target.nextElementSibling;
+    // const previousElement = ev.target.previousElementSibling;
+    // console.log(previousElement);
+    // console.log(nextElement);
+    imageChange(ev.target);
+}
+function imageChange(el){
+    // console.log('element odebrany:');
+    // console.log(el);
+    arrowCheck(el);
+    arrowPrevDiv.addEventListener('click', function(){
+        const img = document.querySelector('.lightbox img');
+        img.src = el.previousElementSibling.src;
+        imageChange(el.previousElementSibling);
+    });
+    arrowNextDiv.addEventListener('click', function(){
+        const img = document.querySelector('.lightbox img');
+        img.src = el.nextElementSibling.src;
+        imageChange(el.nextElementSibling);
+    });
 
 }
-const mainLightbox = document.querySelector('.lightbox');
-mainLightbox.addEventListener('click', hideLightbox);
-function hideLightbox(){
-    mainLightbox.classList.toggle('visible');
+function arrowCheck(el){
+    if(el.previousElementSibling == null){
+        arrowPrevDiv.classList.add('arrowHidden');
+    }
+    else{
+        arrowPrevDiv.classList.remove('arrowHidden');
+    }
+    if(el.nextElementSibling == null){
+        arrowNextDiv.classList.add('arrowHidden');
+    }
+    else{
+        arrowNextDiv.classList.remove('arrowHidden');
+    }
 }
+
+const mainLightbox = document.querySelector('.lightbox');
+mainLightbox.addEventListener('click', hide);
+function hide(event){
+    console.log(event.target);
+    if(event.target.id != 'prev' && event.target.id != 'next')
+        mainLightbox.classList.toggle('visible');
+}
+
+
