b22cf73718fdc4c723f60a858f6306275b2a7bc2
djarosz-hub <jaroszdamian9@gmail.com>
2020-11-02 21:43:43 +0100

    fixing functionalities

--- cw3_notekeep/main1.js
+++ cw3_notekeep/main1.js
@@ -1,67 +1,142 @@
+document.addEventListener('readystatechange', () => {    
+    if (document.readyState == 'complete') {
+        getNotesFromLocalStorage();
+        getAllNotesFromHtml();
+    }
+});
+window.onbeforeunload = synchronize;
+function synchronize(){
+    notes = [];
+    getAllNotesFromHtml();
+    pushNotesToLocalStorage();
+}
 const lsNotesKey = 'notes';
 const newNoteBtn = document.getElementById('newNoteBtn');
 let notes = [];
 const notesContainer = document.querySelector('main');
-newNoteBtn.addEventListener('click',newNoteFunction);
-function newNoteFunction(){
+newNoteBtn.addEventListener('click',newNoteCreateFunction);
+function newNoteCreateFunction(){
     const noteTitle = document.getElementById('noteTitle').value;
     const noteContent = document.getElementById('noteContent').value;
     const noteDate = new Date;
     // console.log(noteTitle);
     // console.log(noteContent);
     // console.log(noteDate);
-
-    const note = {
-        title: noteTitle,
-        content: noteContent,
-        pinned: false,
-        date: noteDate,
-        color: 'default',
-    };
-    notes.push(note);
+    generateNote(noteTitle,noteContent,'unpinned',noteDate,'default', true);
+    // const note = {
+    //     title: noteTitle,
+    //     content: noteContent,
+    //     pinned: false,
+    //     date: noteDate,
+    //     color: 'default',
+    // };
+    // notes.push(note);
 }
 function pushNotesToLocalStorage(){
     localStorage.setItem(lsNotesKey,JSON.stringify(notes));
-    notes = [];
+    //notes = [];
 }
 function getNotesFromLocalStorage(){
     const notesFromLocalStorage = JSON.parse(localStorage.getItem(lsNotesKey));
     const convertedNotes = notesFromLocalStorage.map( note => {
-        note.date = new Date(note.date);
+        note.date = new Date(note.date.toLocaleString());
         return note;
     });
     for(const note of convertedNotes){
         renderNotes(note);
     }
 }
-
+function getAllNotesFromHtml(){
+    //QUERYSELECTORALL CLASS NOTE
+    const inBodyNotes = document.body.querySelectorAll('.note');
+    //const nodes = [].slice.call(inBodyNotes,1);
+    console.log(inBodyNotes);
+    for(const note of inBodyNotes){
+        const noteClass = note.classList.toString().split(' ');
+        //1 = kolor, 2 = pinned
+        const temp = note.children;
+        //console.log(temp);
+        let title = '';
+        let content = '';
+        let date = '';
+        for(let i = 0; i< temp.length;i++){
+            console.log(temp[i].innerHTML);
+            if(i===0){
+                title = temp[i].innerHTML;
+                continue;
+            }
+            if(i===1){
+                content = temp[i].innerHTML;
+                continue;
+            }
+            if(i===2){
+                date = temp[i].innerHTML;
+                break;  
+            }
+        }
+        generateNote(title, content, noteClass[2], date, noteClass[1]);
+    }
+    // if(notes.length > 0)
+    //     return true;
+    // else
+    //     return false;
+    return notes.length > 0 ? true : false; 
+}
+function generateNote(title,content,pin,date,color ='default',toRender = false){
+    const isPinned = pin==='pinned'? true : false;
+    const note = {
+        title: title,
+        content: content,
+        pinned: isPinned,
+        date: new Date(date),
+        color: color,
+    };
+    console.log(note);
+    console.log('to render: ', toRender);
+    if(toRender){
+        renderNotes(note);
+        //pushNotesToLocalStorage();
+        // synchronizeNotes();
+    }
+    else
+        notes.push(note);
+}
 function renderNotes(note){
-    const noteColor;
+    const isPinned = note.pinned?'pinned':'unpinned';
+    let noteColor = '';
     //TODO
     switch(note.color){
-    case 'default':
-        //#808080
-        break;
     case 'olive':
         // #336600
+        noteColor = 'olive';
         break;
     case 'lavend':
+        noteColor = 'lavend';
         //#666699
         break;
     case 'blue':
+        noteColor = 'blue';
         //#336699
         break;
     case 'lightblue':
+        noteColor = 'lightblue';
         //#99ccff
         break;
     case 'sand':
+        noteColor = 'sand';
         //#999966
         break;
     case 'red':
+        noteColor = 'red';
         //#cc0000
         break;
     case 'violet':
+        noteColor = 'violet';
         //#993366
+        break;
+    default:
+        //#808080
+        noteColor = 'default';
         break;
     }
     const htmlNote = document.createElement('section');
@@ -69,7 +144,7 @@ function renderNotes(note){
     const htmlContent = document.createElement('p');
     const htmlTime = document.createElement('time');
     const htmlButton = document.createElement('button');
-    htmlNote.classList.add('note');
+    htmlNote.classList.add('note', noteColor, isPinned);
     htmltitle.innerHTML = note.title;
     htmlContent.innerHTML = note.content;
     htmlTime.innerHTML = note.date.toLocaleString();
@@ -82,6 +157,23 @@ function renderNotes(note){
     notesContainer.appendChild(htmlNote);
 }
 //TODO
+function synchronizeNotes(clean = false){
+    if(clean){
+        notes = [];
+    }
+    getAllNotesFromHtml();
+    console.log('all notes taken');
+    pushNotesToLocalStorage();
+}
 function removeNote(ev){
-    ev.target;
+    const tag = ev.target.parentNode;
+    console.log(tag);
+    tag.parentNode.removeChild(tag);
+    //synchronizeNotes(true);
+    // getAllNotesFromHtml();
+    // console.log('all notes taken');
+    // pushNotesToLocalStorage();
+    //getNotesFromLocalStorage();
+    
+
 }
