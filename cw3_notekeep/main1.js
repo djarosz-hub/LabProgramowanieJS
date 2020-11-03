a4252a9a8cb45ba9648fdcc6936738f49b36c7a7
djarosz-hub <jaroszdamian9@gmail.com>
2020-11-03 17:34:08 +0100

    functionalities fixed

--- cw3_notekeep/main1.js
+++ cw3_notekeep/main1.js
@@ -1,25 +1,29 @@
-document.addEventListener('readystatechange', () => {    
-    if (document.readyState == 'complete') {
-        getNotesFromLocalStorage();
+const lsNotesKey = 'notes';
+document.addEventListener('DOMContentLoaded', () => {    
+    getNotesFromLocalStorage();
+    for(const note of notes){
+        renderNote(note, note.noteId,false);
     }
-});
+}
+);
 window.onbeforeunload = synchronize;
 
 function synchronize(){
-    notes = [];
-    getAllNotesFromHtml();
     pushNotesToLocalStorage();
 }
-const lsNotesKey = 'notes';
 const newNoteBtn = document.getElementById('newNoteBtn');
 let notes = [];
 const notesContainer = document.querySelector('main');
 newNoteBtn.addEventListener('click',newNoteCreateFunction);
 function newNoteCreateFunction(){
-    const noteTitle = document.getElementById('noteTitle').value;
-    const noteContent = document.getElementById('noteContent').value;
-    const noteDate = new Date;
-    generateNote(noteTitle,noteContent,'unpinned',noteDate,'default', true);
+    const noteTitle = document.getElementById('noteTitle');
+    const noteContent = document.getElementById('noteContent');
+    const noteDate = new Date();
+    const noteId = new Date().getTime().toString();
+    generateNote(noteTitle.value,noteContent.value,'unpinned',noteDate,'default',noteId, true);
+    noteTitle.value = '';
+    noteContent.value = '';
+    
 }
 
 function pushNotesToLocalStorage(){
@@ -28,84 +32,35 @@ function pushNotesToLocalStorage(){
 
 function getNotesFromLocalStorage(){
     const notesFromLocalStorage = JSON.parse(localStorage.getItem(lsNotesKey));
-    // for(const note of notesFromLocalStorage){
-    //     console.log(note.date, 'data pobrana');
-    // }
     const convertedNotes = notesFromLocalStorage.map( note => {
-        note.date = new Date(note.date.toLocaleString());
+        note.date = new Date(note.date);
         return note;
     });
-    // for(const note of convertedNotes){
-    //     console.log(note.date, 'data pobrana z converted');
-    // }
-    for(const note of convertedNotes){
-        renderNote(note);
-    }
-}
+    console.log(convertedNotes);
 
-function getAllNotesFromHtml(){
-    const inBodyNotes = document.body.querySelectorAll('.note');
-    console.log(inBodyNotes);
-    for(const note of inBodyNotes){
-        const noteClass = note.classList.toString().split(' ');
-        //1 = kolor, 2 = pinned
-        const temp = note.children;
-        let title = '';
-        let content = '';
-        let date = '';
-        for(let i = 0; i< temp.length;i++){
-            console.log(temp[i].innerHTML);
-            if(i===0){
-                title = temp[i].innerHTML;
-                continue;
-            }
-            if(i===1){
-                content = temp[i].innerHTML;
-                continue;
-            }
-            if(i===2){
-                date = temp[i].innerHTML;
-                break;  
-            }
-        }
-        alert(date);
-        generateNote(title, content, noteClass[2], date, noteClass[1]);
+    for(const note of convertedNotes){
+        notes.push(note);
     }
-    //return notes.length > 0 ? true : false; 
 }
-function generateNote(title,content,pin,date,color ='default',toRender = false){
+function generateNote(title,content,pin,date,color ='default',noteId, toPrepend = false){
     const isPinned = pin==='pinned'? true : false;
-    //const validatedDateFormat = dateCheck(date);
-    //console.log(date,'tutaj poprawiamy');
     const note = {
         title: title,
         content: content,
         pinned: isPinned,
-        date: new Date(date),
+        date: date,
         color: color,
+        noteId: noteId,
     };
-    //console.log(note.date,'data w generate note bez locale');
-    //console.log(note.date.toLocaleString(),'data w generate note z locale');
     console.log(note);
-    console.log('to render: ', toRender);
-    if(toRender){
-        renderNote(note);
-    }
+    renderNote(note,noteId,toPrepend);
+    if(toPrepend)
+        notes.unshift(note);
     else
         notes.push(note);
 }
-// function dateCheck(date){
-//     //const pattern = /^\d{4}[\/\-](0?[1-9]|1[012])[\/\-](0?[1-9]|[12][0-9]|3[01])$/;
-//     const check = date.match(/^\d{4}[\/\-](0?[1-9]|1[012])[\/\-](0?[1-9]|[12][0-9]|3[01])$/);
-//     if(check)
-//         return date;
-//     else{
-//         return date.split('/').reverse().join('-');
-//     }
 
-// }
-
-function renderNote(note){
+function renderNote(note,noteId,toPrepend){
     const isPinned = note.pinned?'pinned':'unpinned';
     let noteColor = '';
     switch(note.color){
@@ -147,20 +102,26 @@ function renderNote(note){
     const htmlContent = document.createElement('p');
     const htmlTime = document.createElement('time');
     const htmlButton = document.createElement('button');
+    htmlNote.id = noteId;
     htmlNote.classList.add('note', noteColor, isPinned);
     htmltitle.innerHTML = note.title;
     htmlContent.innerHTML = note.content;
     htmlTime.innerHTML = note.date.toLocaleString();
-    //console.log(htmlTime.innerHTML,'data w rendernote');
     htmlButton.innerHTML = 'remove';
     htmlButton.addEventListener('click', removeNote);
     htmlNote.appendChild(htmltitle);
     htmlNote.appendChild(htmlContent);
     htmlNote.appendChild(htmlTime);
     htmlNote.appendChild(htmlButton);
-    notesContainer.appendChild(htmlNote);
+    if(toPrepend)
+        notesContainer.prepend(htmlNote);
+    else
+        notesContainer.appendChild(htmlNote);
 }
 function removeNote(ev){
     const tag = ev.target.parentNode;
+    const noteIndex = notes.findIndex(note => note.noteId==tag.id);
+    console.log(noteIndex);
+    notes.splice(noteIndex,1);
     tag.parentNode.removeChild(tag);
 }
