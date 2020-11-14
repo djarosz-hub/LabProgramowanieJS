bffa5ea709eacf94d7c4a42d6e7898d1a1d572ac
djarosz-hub <jaroszdamian9@gmail.com>
2020-11-14 21:22:20 +0100

    hole deleting on collision done

--- ballInHoleApp/main.js
+++ ballInHoleApp/main.js
@@ -18,40 +18,31 @@ const phonePosition = {
     alpha: 0,
     beta: 90,
 };
+let holesArray = createHolesCoords();
 function moveBall(){
-    // ball.speed += 0.1;
     const verticalMove = calculateVerticalMove();
     const horizontalMove = calculateHorizontalMove();
     ball.x +=horizontalMove*ball.speedX;
     ball.y +=verticalMove*ball.speedY;
-    console.log('x',ball.x,'y', ball.y,'speedx',ball.speedX,'speedY',ball.speedY);
+    // console.log(holesArray);
+    //console.log('x',ball.x,'y', ball.y,'speedx',ball.speedX,'speedY',ball.speedY);
     ctx.clearRect(0,0,windowWidth,windowHeight);
+    checkColission(holesArray);
+    drawHoles(ctx,holesArray);
     ctx.beginPath();
-    ctx.arc(ball.x,ball.y,2,0,Math.PI*2);
+    ctx.arc(ball.x,ball.y,10,0,Math.PI*2);
     ctx.fillStyle = '#6DCF00';
     ctx.fill();
 }
 function calculateHorizontalMove(){
     const ballXMove = phonePosition.alpha/60;
-    console.log('horizontal move', ballXMove);
+    //console.log('horizontal move', ballXMove);
     return ballXMove;
 }
 function calculateVerticalMove(){
     const ballYMove = (phonePosition.beta-90)/60;
-    console.log('vertical move', ballYMove);
+    //console.log('vertical move', ballYMove);
     return ballYMove;
-}
-
-moveBall(0,0);
-document.onkeydown = checkKey;
-window.addEventListener('deviceorientation', getPhonePosition);
-function getPhonePosition(ev){
-    console.log(ev.alpha);
-    console.log(ev.beta);
-    phonePosition.alpha = ev.alpha;
-    phonePosition.beta = ev.beta;
-    calculateBallSpeed();
-    moveBall();
 }
 function calculateBallSpeed(){
     const beta = phonePosition.beta;
@@ -59,35 +50,59 @@ function calculateBallSpeed(){
     ball.speedY = 1 + Math.abs((beta-90)/60);
     ball.speedX = 1 + Math.abs(alpha/60);
 }
-function checkKey(e) {
-
-    e = e || window.event;
-
-    if (e.keyCode == '38') {
-        console.log('arrow up');
-        moveBall(0,-2);
+function createHolesCoords(){
+    const arr = [];
+    for(let i = 0; i < 10; i++){
+        let x = Math.random()*windowWidth;
+        let y = Math.random()*windowHeight;
+        //console.log('ball x:', ball.x, 'ball y:', ball.y);
+        if((Math.abs(x - ball.x)) < 50){
+            if((Math.abs(y - ball.y) < 50))
+                y += 70;   
+            else
+                x += 70;
+        }
+        const holeObj = {
+            x:x,
+            y:y,
+            r:15,
+        };
+        arr.push(holeObj);
     }
-    else if (e.keyCode == '40') {
-        console.log('arrow down');
-        moveBall(0,2);
-    }
-    else if (e.keyCode == '37') {
-        console.log('arrow left');
-        moveBall(-2,0);
-    }
-    else if (e.keyCode == '39') {
-        console.log('arrow right');
-        moveBall(2,0);
-    }
-
+    return arr;
 }
-// moveBall();
+function drawHoles(ctx,holesArray){
+    for(const hole of holesArray){
+        ctx.beginPath();
+        ctx.arc(hole.x,hole.y,hole.r,0,Math.PI*2);
+        ctx.fillStyle = 'black';
+        ctx.fill();
+    }
+}
+function checkColission(arr){
+    for(const hole of arr){
+        if(Math.abs(ball.x - hole.x) <= 25 && Math.abs(ball.y - hole.y) <= 25){
+            const index = arr.indexOf(hole);
+            console.log('collision with hole nr:', index);
+            const newArr = holesArray.filter(i => i !== hole);
+            //console.log(newArr);
+            holesArray = newArr;
+            // arr.splice(arr.indexOf(index),1);
+            // console.log(arr.length);
+        }
+    }
+}
 
+moveBall(0,0);
+window.addEventListener('deviceorientation', getPhonePosition);
+function getPhonePosition(ev){
+    //console.log(ev.alpha);
+    //console.log(ev.beta);
+    phonePosition.alpha = ev.alpha;
+    phonePosition.beta = ev.beta;
+    calculateBallSpeed();
+    moveBall();
+}
 setInterval(() => {
     moveBall();
 }, 1000/60);
-
-// ctx.beginPath();
-// ctx.arc(windowWidth/2,windowHeight/2,15,0,Math.PI*2);
-// ctx.fillStyle = '#6DCF00';
-// ctx.fill();
